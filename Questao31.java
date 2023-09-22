b618d0a44104b4f175ade6d05e82937194966ea9
leoalsantosleoalsantos <145804603+leoalsantosleoalsantos@users.noreply.github.com>
2023-09-22 18:26:28 -0300

    Add files via upload

---
+++ Questao31.java
@@ -0,0 +1,14 @@
+    public class Questao31 {
+
+        public static void main(String[] args) {
+
+            int numero = 100;
+            int quant = 1000;
+
+            for (int i = 0; i < quant; i++) {
+                System.out.println(numero);
+            }
+
+
+        }
+}
---
+++ Questao7.java
@@ -0,0 +1,23 @@
+public class Questao7 {
+
+    public static void main(String[] args) {
+
+        conta(97);
+
+    }
+
+    static void conta(int conta){
+        System.out.println(conta + "R$ é o total da conta doCliente");
+        double comissão = ((double)conta) / 10;
+        System.out.println("Comissão do Garçom "+ comissão+"R$");
+
+
+
+
+
+
+    }
+
+
+
+}
